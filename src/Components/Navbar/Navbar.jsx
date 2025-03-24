e99aefc25df646cc1f707435b426e902b0570c22
NOURAN <nouranmady56@gmail.com>
2025-03-24 05:32:42 +0200

    Navbar style done without any action & without menubar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,18 +5,18 @@ import './_Navbar.scss';
 function Navbar() {
     return <>
     
-    <nav>
-            <div className="container">
-                <div className="d-flex justify-content-between align-items-center ">
+    <nav className='fw-bold'>
+            <div className="container d-flex justify-content-between align-items-center">
+                
                     <div className="links-left d-none d-lg-flex column-gap-4">
                         <Link to=''  className="activeLink">Home</Link>
                         <Link to=''>For Company</Link>
                         <Link to=''>About Us</Link>
                     </div>
 
-                    <div className="head pt-1">
+                    <div className="heading pt-1 fw-light text-center">
                         <Link to=''>
-                            <h1>JOB HUB</h1>
+                            <h1 className='fw-light border-bottom border-dark pb-2'>JOB HUB</h1>
                             <p>For Fresh Graduates</p>
                         </Link>
                     </div>
@@ -26,7 +26,7 @@ function Navbar() {
                             <i className="fa-solid fa-magnifying-glass"></i>
                         </a>
                         
-                        <a href='l' className="add-job align-content-center">Add Job</a>
+                        <a href='l' className="add-job align-content-center fw-semibold text-center rounded text-white">Add Job</a>
                         <a href="login.html">Login</a>
                         <a href='l' className="menu-bar">
                             <i className="fa-solid fa-bars"></i>
@@ -37,7 +37,7 @@ function Navbar() {
                             <i className="fa-solid fa-bars"></i>
                         </a>
                         </div>
-                </div>
+                
             </div>
         </nav>
 
